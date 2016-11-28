dc31e14b6c6b962a1df50f201c0a98f10a07cf84
Knowledge <dirkhornung91@gmail.com>
2016-11-28 15:33:04 +0100

    AngularJS: 10 - More Templating

---
+++ AngularJS/angular-phonecat/app/phone-detail/phone-detail.component.js
@@ -0,0 +1,13 @@
+angular.
+    module('phoneDetail').
+    component('phoneDetail', {
+        templateUrl: 'phone-detail/phone-detail.template.html',
+        controller: ['$routeParams', '$http',
+            function PhoneDetailController($routeParams, $http) {
+                var self = this;
+                $http.get('phones/' + $routeParams.phoneId + '.json').then(function(response) {
+                    self.phone = response.data;
+                });
+            }
+        ]
+    });
