be64945fb70d2f3e494f58a4eb43f5197e748f76
Knowledge <dirkhornung91@gmail.com>
2016-12-12 14:58:17 +0100

    Completed CMake2

---
+++ CMake2/Example5/build/CMakeFiles/libtest.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/Knowledge/Developer/Tutorials/CMake2/Example5/libtest.cpp" "/Users/Knowledge/Developer/Tutorials/CMake2/Example5/build/CMakeFiles/libtest.dir/libtest.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/Knowledge/Developer/Tutorials/CMake2/Example3/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
